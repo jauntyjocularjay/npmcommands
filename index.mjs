0eafa51a8b799bfada0ab1f1fa8998d039dbaed5
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-06-04 09:30:55 -0700

    update index.mjs with updated imports

--- index.mjs
+++ index.mjs
@@ -14,21 +14,90 @@ import {
     Img,
     SpriteSheet,
 } from './phaserhelpers/index.mjs'
-import {} from './vjsc.vanilla.mjs'
+import {
+    // Constants
+    display,
+    flex,
+    event,
+    unit,
+    tag,
+
+    // Base Classes
+    OptionSelection as Selection,
+    StyleSheet,
+    Listener,
+    ListenerOnLoad,
+    // FlexBoxClass,
+
+    // Classables
+    // // Containers
+    Img,
+    Div,
+    DivBtn,
+    FlexBox,
+    Figure,
+    Form,
+    Label,
+    // // Input
+    Button,
+    Input,
+    TextArea,
+    Select,
+    Option,
+    // // Format elements
+    Br,
+    //  // External Resource Links
+    Link,
+    Style,
+
+    // Text Elements
+    // // Headers
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    // // Body Text
+    P,
+    PSpan,
+    Figcaption,
+    A,
+    Strong,
+    Abbr,
+    Blockquote,
+    Sub,
+    Sup,
+    Span,
+    Text,
+    Code,
+    Pre,
+
+    // Functions
+    getStylesheetByFileName,
+    addAdoptedStyleSheet,
+
+    // JSONCSS
+    JSONCSS,
+    UnsupportedJSONCSSError,
+    PercentageOutOfRangeError
+} from './vjsc.vanilla.mjs'
 
 let scene = this
 
-let config = new Config(new View(800, 600), PHYSICS.arcade, scene)
+let config = new Config(new View(800, 600), PHYSICS, scene)
 
 function preload ()
 {
-
+    
 }
 
 function create ()
 {
+
 }
 
 function update ()
 {
+
 }
